71d92fd233cf7b25ec170f470df53f3f2a273903
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-10-26 23:06:02 +0600

    course card added

--- src/Layout/CoursePageLayout.js
+++ src/Layout/CoursePageLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet, useLoaderData } from "react-router-dom";
 import { Row, Col, Container } from 'react-bootstrap';
+import CardGroup from 'react-bootstrap/CardGroup';
 import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 import Course from '../Pages/Course/Course';
 
@@ -12,13 +13,16 @@ const CoursePageLayout = () => {
         <div>
             <Container fluied className=''>
                 <Row>
-                    <Col lg='4' className='d-none d-lg-block'>
+                    <Col lg='3' className='d-none d-lg-block'>
                         <LeftSideNav course={courses}></LeftSideNav>
                     </Col>
-                    <Col lg='8'>
+                    <Col  lg='9'>
+                    <Row xs={1} sm={1} md={2} lg={3} xl={3} className="g-4">
                         {
                             courses.map( course => <Course key={course._id} course={course}></Course>)
-                        }
+                        }                      
+                    </ Row>
+
                     </Col>
                 </Row>
             </Container>
--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import './Course.css'
 
 const Course = ({course}) => {
-    const courseCard = course
-    console.log(courseCard);
+    const {instructor, duration, title, picture, price} = course;
+
     return (
-        <div>
-            <h2>Course</h2>
-        </div>
+        <Col>
+          <Card>
+            <Card.Img className='course-img' variant="top" src={picture} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <p>{instructor}</p>
+          </Card.Body>
+          <Card.Footer className='d-flex justify-content-between'>
+            <small className="text-muted">Price: {price}</small>
+            <small className="text-muted">Duration: {duration}</small>
+          </Card.Footer>
+          </Card>            
+        </Col>
+
     );
 };
 

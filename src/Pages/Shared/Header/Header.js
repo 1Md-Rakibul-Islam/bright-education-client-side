aaa2b19bbcb12f0f9e96777bc2e3eb656b2b9ff3
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-10-27 23:44:53 +0600

    button

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/esm';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import './Header.css'
+import { FaUserCircle } from "react-icons/fa";
 
 
 const Header = () => {
@@ -32,12 +34,14 @@ const Header = () => {
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                   <Nav className="text-none justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link><Button>Dark</Button></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/home"}>Home</Link></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/course"}>Course</Link></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/faq"}>FAQ</Link></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/blogs"}>Blogs</Link></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/login"}>Login</Link></Nav.Link>
                     <Nav.Link ><Link className='text-decoration-none' to={"/register"}>Register</Link></Nav.Link>
+                    <FaUserCircle></FaUserCircle>
                   </Nav>
                 </Offcanvas.Body>
               </Navbar.Offcanvas>
